e4e4e7512a4c91e5fd7030f2e84c1684fb2518e2
elmaroufa <elmaroufa@gmail.com>
2022-01-31 10:40:24 +0100

    end edit test home page

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 class HomePagesTest(SimpleTestCase):
 
@@ -14,6 +15,23 @@ class HomePagesTest(SimpleTestCase):
     def test_homepage_template(self): # new
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_containts_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self): 
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
 
 
 
