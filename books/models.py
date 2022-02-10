938ea2f220afef01c0027ddcbe8ff6466b4200eb
elmaroufa <elmaroufa@gmail.com>
2022-02-10 17:46:23 +0100

    system review links book model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,9 @@
+from tkinter import CASCADE
 import uuid
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 
 
 class Book(models.Model):
@@ -18,3 +21,18 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    review = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.review
