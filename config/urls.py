16103c42d78f9642083300a46ebce2254c618c74
elmaroufa <elmaroufa@gmail.com>
2022-02-12 21:12:53 +0100

    add system upload image media

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -21,5 +23,4 @@ urlpatterns = [
     path('accounts/',include('allauth.urls') ),
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
